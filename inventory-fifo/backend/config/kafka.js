f72456388b80e73439ca3fafb1d1acf31a5b3f04
ssaurabh3449 <3449ssingh@gmail.com>
2025-10-31 23:11:02 +0530

    Update kafka.js

--- inventory-fifo/backend/config/kafka.js
+++ inventory-fifo/backend/config/kafka.js
@@ -2,14 +2,15 @@ require('dotenv').config();
 const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-  clientId: 'inventory-fifo-simulator',
+  clientId: 'inventory-fifo',
   brokers: [process.env.REDPANDA_BROKER],
   ssl: true,
   sasl: {
     mechanism: process.env.REDPANDA_SASL_MECHANISM,
     username: process.env.REDPANDA_USERNAME,
     password: process.env.REDPANDA_PASSWORD
-  }
+  },
+    logLevel: logLevel.DEBUG
 });
 
 module.exports = kafka;
